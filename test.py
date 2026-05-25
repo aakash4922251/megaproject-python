f91a036bfe91f97f4ecae4382d3e8a1cda59bb84
Nair <an97970@win.edag.de>
2026-05-25 17:43:58 +0530

    ui-layer test

--- test.py
+++
@@ -1,8 +0,0 @@
-'''
-Run with:
-    python -m streamlit run test.py
-'''
-
-import streamlit as st
-
-st.header("EV BMS Data Analysis")
---
+++ ui.py
@@ -0,0 +1,15 @@
+'''
+Run with:
+    python -m streamlit run dashboard.py
+'''
+
+import streamlit as st
+import pandas as pd
+
+st.title("EV BMS Data Analysis")
+
+df = pd.read_csv("data/bms_data.csv")
+
+#show battery data
+st.header("Battery Data")
+st.dataframe(df)
